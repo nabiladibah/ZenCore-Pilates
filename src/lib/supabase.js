36e93c8a565a4f6ee6792261e399f97ebd70edeb
Nabila Adibah Larasati <nabilaadibahlarasati@Dibahs-MacBook.local>
2026-06-12 20:47:40 +0700

    add supabase connection

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://goszgfwegmqgvnguyvpf.supabase.co',
+  'sb_publishable_09b36iNLL5Z1MT96U-faSQ_jXZQLkhJ'
+)
